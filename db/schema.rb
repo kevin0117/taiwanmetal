db8e51c56f52c322d1236dd437c93a898e6909f7
Kevin Wang <kevin0117@gmail.com>
2020-06-05 00:25:01 +0800

    Complete Product_Lists & Basic CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_053804) do
+ActiveRecord::Schema.define(version: 2020_06_04_161443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_lists", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.text "description"
+    t.boolean "online", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "scraps", force: :cascade do |t|
     t.string "title"
