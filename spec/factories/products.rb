d827ca032cdd837361187e0c969b2c08e018bd81
Kevin Wang <kevin0117@gmail.com>
2020-06-05 23:13:50 +0800

    append rspec tests for Model Product

---
+++ spec/factories/products.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :product do
+    title { Faker::Commerce.product_name }
+    weight { Faker::Number.decimal(l_digits: 2) }
+    cost { Faker::Number.between(from: 0, to: 980) }
+    service_fee { Faker::Number.between(from: 150, to: 1280) }
+    barcode { Faker::Code.nric }
+    on_sell { false }
+    code { Faker::Code.nric }
+    deleted_at { "2020-06-05 13:16:54" }
+
+    vendor
+    product_list
+    user
+  end
+end
