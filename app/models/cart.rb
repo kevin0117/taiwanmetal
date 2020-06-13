2bf4f598bbad04b3dbe9f314241ef1666d3ae1f1
Kevin Wang <kevin0117@gmail.com>
2020-06-13 15:29:13 +0800

    Preparing two PORO (Cart & CartItem) for building sale feature

---
+++ app/models/cart.rb
@@ -0,0 +1,62 @@
+class Cart
+  attr_reader :items
+
+  def initialize(items = [])
+    @items = items
+  end
+
+  def add_product(product)
+    current_item = items.find { |item| item.product_id == product.id }
+    
+    if current_item
+      current_item.increment
+    else
+      @items << CartItem.new(product.id)
+    end
+  end
+
+  def remove_product(product)
+    current_item = items.find{ |item| item.product_id == product.id }
+
+    if current_item
+      @items.delete(current_item)
+    end
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+  def total_price
+    items.reduce(0) { |sum, item| sum + item.total_price }
+  end
+
+  def serialize
+    all_items = @items.map {|item|
+      { product_id: item.product_id, quantity: item.quantity }
+    }
+
+    { items: all_items }
+  end
+
+  def cart_hash
+    {
+      items: [
+        {product_id: product1.id, quantity: 3},
+        {product_id: product2.id, quantity: 5},
+      ]
+    }
+  end
+
+  def self.restore_hash(hash = nil)
+    if hash && hash[:items]
+      items = hash[:items].map { |item| 
+        CartItem.new(item[:product_id], item[:quantity])
+      }
+      Cart.new(items)
+    else
+      Cart.new
+    end  
+  end
+end
+
