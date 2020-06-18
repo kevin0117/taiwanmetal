1beb4c6f7fa250591dcb3ccc523f9bb4c612c5ed
Kevin Wang <kevin0117@gmail.com>
2020-06-18 14:40:26 +0800

    append rspec tests for Cart and CartItem

---
+++ spec/models/cart_item_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+# 參考自 "為你自己學 Ruby on Rails"
+RSpec.describe CartItem, type: :model, tag_item: true do
+  describe "CartItem 基本功能" do
+    let(:pb) { FactoryBot.create(:price_board, gold_selling: 5000.0) }
+    let(:product1) { FactoryBot.create(:product, price_board: pb, weight: 1.0, service_fee: 800.0) }
+    let(:product2) { FactoryBot.create(:product, price_board: pb, weight: 2.0, service_fee: 200.0) }
+    let(:cart) { Cart.new }
+
+    it "每個 Cart Item 都可以計算它自己的金額（小計)" do
+      c1 = CartItem.new(product1.id, 2)
+      
+      3.times { cart.add_product(product1) }  # $17400
+      2.times { cart.add_product(product2) }  # $20400
+
+      expect(cart.items.first.total_price).to eq 17400
+      expect(cart.items.last.total_price).to eq 20400
+    end
+  end
+end
