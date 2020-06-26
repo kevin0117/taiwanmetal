793d4db1cbf796b747ac1f14e0554b6fe0f3894f
Kevin Wang <kevin0117@gmail.com>
2020-06-26 23:03:54 +0800

    Add common methods among controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_cart, :find_quantity
+
   def generate_barcode(text, id)
       barcode = Barby::Code39.new(text, true)
       outputter = Barby::PngOutputter.new(barcode)
       File.open("#{Rails.root}/public/barcode-#{id}.png", 'wb'){|f| f.write outputter.to_png }
+  end
+
+  def current_cart
+    @cart = @cart || Cart.restore_hash(session[:cart0117])
+  end
+
+  def find_quantity(product)
+    current_cart.items.find{|item| item.product_id == product.id}.quantity
   end
 end
