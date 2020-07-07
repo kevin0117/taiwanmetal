2f0841bd2452812568751f3adc6a3626f7e3eb4a
Kevin Wang <kevin0117@gmail.com>
2020-07-07 16:43:42 +0800

    relocate barcode image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def generate_barcode(text, id)
     barcode = Barby::Code39.new(text, true)
     outputter = Barby::PngOutputter.new(barcode)
-    ​File.open("#{Rails.root}/app/assets/images/barcode-#{id}.png", 'wb'){|f| f.write outputter.to_png }
+    File.open("#{Rails.root}/public/barcode-#{id}.png", 'wb'){|f| f.write outputter.to_png }
   end
 
   def current_cart
