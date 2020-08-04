a930136f18a8d9afb1f91fc16af0cad7f70203a7
Kevin Wang <kevin0117@gmail.com>
2020-08-04 10:43:59 +0800

    adding comments and rephrase the variable

--- app/models/price_board.rb
+++ app/models/price_board.rb
@@ -1,31 +1,39 @@
 class PriceBoard < ApplicationRecord
   belongs_to :user
 
+  # If the date is not passing through, the default value of date will be set to today 
+  # it will return for today's price 
+
+  # If the date is passing through with nil value, the price_boards will be empty
+  # It will return the latest price in recorded in the database 
+  
+  # If the date is set and can be found in the database, 
+  # it will return the price according to the date
+
   def self.find_gold_selling(id, date = Date.today)
     date = date.to_s.split(' ').first
 
-    target_date = PriceBoard.select{ |item| 
+    price_boards = PriceBoard.select{ |item| 
       item.price_date.to_s == date && item.user_id == id
     }  
-    if target_date.empty?
+    if price_boards.empty?
       PriceBoard.last.gold_selling if PriceBoard.last.present?
     else
-      target_date.first.gold_selling
+      price_boards.first.gold_selling
     end
   end
-
-
 
   def self.find_gold_buying(id, date = Date.today)
     date = date.to_s.split(' ').first
 
-    target_date = PriceBoard.select{ |item| 
+    price_boards = PriceBoard.select{ |item| 
       item.price_date.to_s == date && item.user_id == id
     }  
-    if target_date.empty?
+    
+    if price_boards.empty?
       PriceBoard.last.gold_buying if PriceBoard.last.present?
     else
-      target_date.first.gold_buying
+      price_boards.first.gold_buying
     end
   end
   
