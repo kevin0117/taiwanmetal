333ec2966b367074efc4c950cc5134a0996f661c
Kevin Wang <kevin0117@gmail.com>
2020-08-29 01:45:14 +0800

    update files to make "cancel" & "deal" actions perform asynchronously

--- app/javascript/channels/board_channel.js
+++ app/javascript/channels/board_channel.js
@@ -10,13 +10,22 @@ consumer.subscriptions.create("BoardChannel", {
     // Called when there's incoming data on the websocket for this channel
     
     const commodityContainer = document.getElementById('commodities')
-    const commodityRecord = document.getElementById(data.deal)
+    const closed_commodityContainer = document.getElementById('closed_commodities')
+    const commodityRecords = document.getElementsByClassName(data.deal)
+    
+    // data['deal'] is equal to data.deal
+    // if the "deal" key can't be find in data hash, javascript will return "undefined" 
+    // it means the create action is required, and vice versa 
+    if (data['deal']!= undefined) { 
+      var i = 0;
+      do {
+        commodityRecords[i].innerText='';
+        i += 1;
+        console.log(i);
+      } while (i < commodityRecords.length);
+      closed_commodityContainer.innerHTML = data.closed + closed_commodityContainer.innerHTML; 
 
-    // console.log(data.deal)
-
-    // data['deal'] is equal to data.deal 
-    if (data['deal']!= undefined) 
-      commodityRecord.innerText='';      
+    }
     else
       commodityContainer.innerHTML = data.html + commodityContainer.innerHTML; 
   }
