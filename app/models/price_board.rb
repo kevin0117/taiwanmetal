d81a29d3a26ddc979b17d38a73065085f423e737
Kevin Wang <kevin0117@gmail.com>
2021-01-18 21:48:35 +0800

    comment out the unused code and tests for now

--- app/models/price_board.rb
+++ app/models/price_board.rb
@@ -1,47 +1,46 @@
 class PriceBoard < ApplicationRecord
   belongs_to :user
-  
-  validates :wholesale_gold_selling, 
+
+  validates :wholesale_gold_selling,
             :wholesale_gold_buying,
             :gold_selling,
             :gold_buying, presence: true
 
-  # If the date is not passing through, the default value of date will be set to today 
+  # If the date is not passing through, the default value of date will be set to today
   # it will return for today's price if today's price exists
 
   # If the date is passing through with nil value, the price_boards will be empty
-  # It will return all zeros because 1st record of PriceBoard is hardcoded 
+  # It will return all zeros because 1st record of PriceBoard is hardcoded
   # in the database with all zeros
-  
-  # If the date is set and can be found in the database, 
+
+  # If the date is set and can be found in the database,
   # it will return the price according to the date
 
-  def self.find_gold_selling(id, date = Date.today)
-    date = date.to_s.split(' ').first
+  # def self.find_gold_selling(id, date = Date.today)
+  #   date = date.to_s.split(' ').first
 
-    price_boards = PriceBoard.select{ |item| 
-      item.price_date.to_s == date && item.user_id == id
-    }  
-    
-    if price_boards.empty?
-      PriceBoard.first.gold_selling
-    else
-      price_boards.first.gold_selling
-    end
-  end
+  #   price_boards = PriceBoard.select{ |item|
+  #     item.price_date.to_s == date && item.user_id == id
+  #   }
 
-  def self.find_gold_buying(id, date = Date.today)
-    date = date.to_s.split(' ').first
+  #   if price_boards.empty?
+  #     PriceBoard.first.gold_selling
+  #   else
+  #     price_boards.first.gold_selling
+  #   end
+  # end
 
-    price_boards = PriceBoard.select{ |item| 
-      item.price_date.to_s == date && item.user_id == id
-    }  
-    
-    if price_boards.empty?
-      PriceBoard.first.gold_buying
-    else
-      price_boards.first.gold_buying
-    end
-  end
-  
+  # def self.find_gold_buying(id, date = Date.today)
+  #   date = date.to_s.split(' ').first
+
+  #   price_boards = PriceBoard.select{ |item|
+  #     item.price_date.to_s == date && item.user_id == id
+  #   }
+
+  #   if price_boards.empty?
+  #     PriceBoard.first.gold_buying
+  #   else
+  #     price_boards.first.gold_buying
+  #   end
+  # end
 end
--- spec/models/price_board_spec.rb
+++ spec/models/price_board_spec.rb
@@ -49,67 +49,67 @@ RSpec.describe PriceBoard, type: :model, price: true do
       expect(pb).not_to be_valid
       expect { expect(pb).to be_valid }.to raise_exception(/Gold buying 不能為空白/)
     end
-    it '黃金價格(賣出)可依照當天日期及個別使用者來預設填寫' do
-      pb1 = FactoryBot.create(:price_board, price_date: '2020-09-11',
-                                            user_id: user1.id, gold_selling: 6000)
+    # it '黃金價格(賣出)可依照當天日期及個別使用者來預設填寫' do
+    #   pb1 = FactoryBot.create(:price_board, price_date: '2020-09-11',
+    #                                         user_id: user1.id, gold_selling: 6000)
 
-      pb2 = FactoryBot.create(:price_board, price_date: '2020-09-11',
-                                            user_id: user2.id, gold_selling: 5000)
+    #   pb2 = FactoryBot.create(:price_board, price_date: '2020-09-11',
+    #                                         user_id: user2.id, gold_selling: 5000)
 
-      pb3 = FactoryBot.create(:price_board, price_date: '2020-09-12',
-                                            user_id: user1.id, gold_selling: 4000)
+    #   pb3 = FactoryBot.create(:price_board, price_date: '2020-09-12',
+    #                                         user_id: user1.id, gold_selling: 4000)
 
-      expect(PriceBoard.find_gold_selling(user1.id, '2020-09-11') == 6000).to be_truthy
-      expect(PriceBoard.find_gold_selling(user1.id, '2020-09-12') == 4000).to be_truthy
-      expect(PriceBoard.find_gold_selling(user2.id, '2020-09-11') == 6000).to be_falsy
-    end
-    it '黃金價格(買進)可依照當天日期及個別使用者來預設填寫' do
-      pb1 = FactoryBot.create(:price_board, price_date: '2020-09-11',
-                                            user_id: user1.id, gold_buying: 6000)
+    #   expect(PriceBoard.find_gold_selling(user1.id, '2020-09-11') == 6000).to be_truthy
+    #   expect(PriceBoard.find_gold_selling(user1.id, '2020-09-12') == 4000).to be_truthy
+    #   expect(PriceBoard.find_gold_selling(user2.id, '2020-09-11') == 6000).to be_falsy
+    # end
+    # it '黃金價格(買進)可依照當天日期及個別使用者來預設填寫' do
+    #   pb1 = FactoryBot.create(:price_board, price_date: '2020-09-11',
+    #                                         user_id: user1.id, gold_buying: 6000)
 
-      pb2 = FactoryBot.create(:price_board, price_date: '2020-09-11',
-                                            user_id: user2.id, gold_buying: 5000)
+    #   pb2 = FactoryBot.create(:price_board, price_date: '2020-09-11',
+    #                                         user_id: user2.id, gold_buying: 5000)
 
-      pb3 = FactoryBot.create(:price_board, price_date: '2020-09-12',
-                                            user_id: user1.id, gold_buying: 4000)
+    #   pb3 = FactoryBot.create(:price_board, price_date: '2020-09-12',
+    #                                         user_id: user1.id, gold_buying: 4000)
 
-      expect(PriceBoard.find_gold_buying(user1.id, '2020-09-11') == 6000).to be_truthy
-      expect(PriceBoard.find_gold_buying(user1.id, '2020-09-12') == 4000).to be_truthy
-      expect(PriceBoard.find_gold_buying(user2.id, '2020-09-11') == 6000).to be_falsy
-    end
+    #   expect(PriceBoard.find_gold_buying(user1.id, '2020-09-11') == 6000).to be_truthy
+    #   expect(PriceBoard.find_gold_buying(user1.id, '2020-09-12') == 4000).to be_truthy
+    #   expect(PriceBoard.find_gold_buying(user2.id, '2020-09-11') == 6000).to be_falsy
+    # end
 
-    it '如果『每日金價未建立』或是『使用者不存在』, 黃金價格(賣出)將預設為 0' do
-      # 建立ㄧ筆貴金屬價格測試假資料
-      default = PriceBoard.create(
-        wholesale_gold_selling: 0, wholesale_gold_buying: 0,
-        gold_selling: 0, gold_buying: 0,
-        price_date: '2020-01-01', user_id: user1.id
-      )
-      pb1 = FactoryBot.create(
-        :price_board, price_date: '2020-09-12',
-                      user_id: user1.id, gold_selling: 6000
-      )
+    # it '如果『每日金價未建立』或是『使用者不存在』, 黃金價格(賣出)將預設為 0' do
+    #   # 建立ㄧ筆貴金屬價格測試假資料
+    #   default = PriceBoard.create(
+    #     wholesale_gold_selling: 0, wholesale_gold_buying: 0,
+    #     gold_selling: 0, gold_buying: 0,
+    #     price_date: '2020-01-01', user_id: user1.id
+    #   )
+    #   pb1 = FactoryBot.create(
+    #     :price_board, price_date: '2020-09-12',
+    #                   user_id: user1.id, gold_selling: 6000
+    #   )
 
-      expect(PriceBoard.find_gold_selling(user1.id, '2020-09-12') == 6000).to be_truthy
-      expect(PriceBoard.find_gold_selling(user1.id) == 0.0).to be_truthy
-      expect(PriceBoard.find_gold_selling(user2.id, '2020-09-12') == 0.0).to be_truthy
-    end
-    it '如果『每日金價未建立』或是『使用者不存在』, 黃金價格(買進)將預設為 0' do
-      # 資料庫內建第一筆所有貴金屬價格資料為零
-      default = PriceBoard.create(
-        wholesale_gold_selling: 0, wholesale_gold_buying: 0,
-        gold_selling: 0, gold_buying: 0,
-        price_date: '2020-01-01', user_id: user1.id
-      )
-      # 建立ㄧ筆貴金屬價格測試假資料
-      pb1 = FactoryBot.create(
-        :price_board, price_date: '2020-09-12',
-                      user_id: user1.id, gold_buying: 6000
-      )
+    #   expect(PriceBoard.find_gold_selling(user1.id, '2020-09-12') == 6000).to be_truthy
+    #   expect(PriceBoard.find_gold_selling(user1.id) == 0.0).to be_truthy
+    #   expect(PriceBoard.find_gold_selling(user2.id, '2020-09-12') == 0.0).to be_truthy
+    # end
+    # it '如果『每日金價未建立』或是『使用者不存在』, 黃金價格(買進)將預設為 0' do
+    #   # 資料庫內建第一筆所有貴金屬價格資料為零
+    #   default = PriceBoard.create(
+    #     wholesale_gold_selling: 0, wholesale_gold_buying: 0,
+    #     gold_selling: 0, gold_buying: 0,
+    #     price_date: '2020-01-01', user_id: user1.id
+    #   )
+    #   # 建立ㄧ筆貴金屬價格測試假資料
+    #   pb1 = FactoryBot.create(
+    #     :price_board, price_date: '2020-09-12',
+    #                   user_id: user1.id, gold_buying: 6000
+    #   )
 
-      expect(PriceBoard.find_gold_buying(user1.id, '2020-09-12') == 6000).to be_truthy
-      expect(PriceBoard.find_gold_buying(user1.id) == 0.0).to be_truthy
-      expect(PriceBoard.find_gold_buying(user2.id, '2020-09-12') == 0.0).to be_truthy
-    end
+    #   expect(PriceBoard.find_gold_buying(user1.id, '2020-09-12') == 6000).to be_truthy
+    #   expect(PriceBoard.find_gold_buying(user1.id) == 0.0).to be_truthy
+    #   expect(PriceBoard.find_gold_buying(user2.id, '2020-09-12') == 0.0).to be_truthy
+    # end
   end
 end
