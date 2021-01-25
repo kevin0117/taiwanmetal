60510008bc30b6a2c54cf77cf8bd25be88d70b5f
Kevin Wang <kevin0117@gmail.com>
2021-01-26 01:59:09 +0800

    Plain Old Ruby Object for qrcode

---
+++ app/models/qr_code.rb
@@ -0,0 +1,34 @@
+class QrCode
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def self.call(url)
+    new(url).call
+  end
+
+  def call
+    qrcode = RQRCode::QRCode.new(url)
+    qrcode.as_svg(
+      offset: 0,
+      color: '000000',
+      module_size: 3,
+      standalone: true
+    ).html_safe
+  end
+end
+
+
+# class QrCode
+#   def self.call(url_path)
+#     qrcode = RQRCode::QRCode.new(url_path)
+#     qrcode.as_svg(
+#       offset: 0,
+#       color: '000000',
+#       module_size: 3,
+#       standalone: true
+#     ).html_safe
+#   end
+# end
