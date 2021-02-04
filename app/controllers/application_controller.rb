cde6237309e4de52af75385286f5f61c76a05fab
Kevin Wang <kevin0117@gmail.com>
2021-02-05 01:30:05 +0800

    handle RecordNotFound error on application controller level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   around_action :switch_locale
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
 
   # 設定語系
   def set_locale
@@ -44,6 +46,14 @@ class ApplicationController < ActionController::Base
 
   def find_quantity(product)
     current_cart.items.find{|item| item.product_id == product.id}.quantity
+  end
+
+  private
+
+  def record_not_found
+    render file: "#{Rails.root}/public/404.html",
+    layout: false,
+    status: 404
   end
 
   protected
