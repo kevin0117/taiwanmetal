f728ff6b73f56277a354cd64ee261d7cdc8ea161
Kevin Wang <kevin0117@gmail.com>
2021-03-04 21:02:35 +0800

    configure session controller for forbidding users to logout when current_cart or refining_cart is not empty

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  def new
+    super
+  end
+
+  # POST /resource/sign_in
+  def create
+    super
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    if refining_cart.empty? &&  current_cart.empty?
+      super
+    else
+      redirect_back fallback_location: root_url, notice: "銷售單/提煉單 未完成"
+    end
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+end
