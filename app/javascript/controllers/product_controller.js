35ef1795f0f452b762d5d287090b8788781a4ecc
Kevin Wang <kevin0117@gmail.com>
2021-03-10 03:01:52 +0800

    let stimulus do the calculation for us

---
+++ app/javascript/controllers/product_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "weight", "service_fee", "total_weight", "quantity", "total_service_fee" ]
+
+  cal_total_weight(event) {
+    event.preventDefault();
+
+    let weight = this.weightTarget.value
+    let service_fee = this.service_feeTarget.value
+    let quantity = this.quantityTarget.value
+
+    let total_weight = (weight * quantity).toFixed(2)
+    let total_service_fee = (service_fee * quantity).toFixed(2)
+
+    console.log(weight)
+    console.log(total_service_fee)
+    console.log(quantity)
+    console.log(total_weight)
+
+    this.total_weightTarget.value = total_weight
+    this.total_service_feeTarget.value = total_service_fee
+  }
+}
