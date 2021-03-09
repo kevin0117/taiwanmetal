884ba36c5a3c858052f388b3b2c9455bc30edded
Kevin Wang <kevin0117@gmail.com>
2021-03-10 03:02:03 +0800

    layout adjust

--- db/schema.rb
+++ db/schema.rb
@@ -258,12 +258,6 @@ ActiveRecord::Schema.define(version: 2021_03_09_054950) do
     t.index ["user_id"], name: "index_vendors_on_user_id"
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "commodities", "users"
   add_foreign_key "customers", "users"
