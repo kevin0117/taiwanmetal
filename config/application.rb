8653b0a846e099461824575de7a3065e99bf229a
Kevin Wang <kevin0117@gmail.com>
2021-03-09 14:09:13 +0800

    layout adjust

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Taiwanmetal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = "zh-TW"
+    config.time_zone = 'Taipei'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
